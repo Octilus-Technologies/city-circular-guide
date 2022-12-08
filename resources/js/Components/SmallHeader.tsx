93b3ddef294e75ab22dbe04d2fbfb317e7d916e8
Amit Hazra <ahazratwt@gmail.com>
2022-12-09 00:12:46 +0530

    fixed title

--- resources/js/Components/SmallHeader.tsx
+++ resources/js/Components/SmallHeader.tsx
@@ -9,7 +9,7 @@ function SmallHeader() {
                     <div className="icon inline-block h-[36px] w-[36px] rounded-full bg-secondary"></div>
                     <div className="name flex flex-col items-start font-bold">
                         <h1 className="text-xl capitalize text-primary-content">
-                            City Circular Project
+                            City Circular Guide
                         </h1>
                         <span className="-mt-1 capitalize text-secondary">
                             Trivandrum
