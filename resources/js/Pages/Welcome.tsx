5dd9f5526f4c778cba52e29a58992c5edcd18963
Amit Hazra <ahazratwt@gmail.com>
2022-12-20 19:08:51 +0530

    refactored welcome page

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,38 +1,13 @@
 import InitialRouteQueryForm from "@/Components/forms/InitialRouteQueryForm";
 import BusRouteMap from "@/Components/map/BusRouteMap";
 import SideBar from "@/Components/SideBar";
-import {
-    circularColors,
-    CircularName,
-    circulars,
-    Coordinates,
-    generateLayerFromGeometry,
-    getCircularCoordinates,
-    getStopDetails,
-} from "@/utils/geoJson";
-import { getMatch } from "@/utils/mapbox-api";
-import { Head, useRemember } from "@inertiajs/inertia-react";
+import useCirculars from "@/utils/hooks/userCirculars";
+import { Head } from "@inertiajs/inertia-react";
 import "mapbox-gl/dist/mapbox-gl.css";
-import React, { Fragment, useEffect, useMemo, useState } from "react";
+import React, {
+    Fragment, useState
+} from "react";
 import { Layer, LayerProps, Source } from "react-map-gl";
-
-type CircularData = {
-    name: CircularName;
-    color: string;
-    path: {
-        type: string;
-        features: {
-            type: string;
-            properties: Object;
-            geometry: Object;
-        }[];
-    };
-    stops: {
-        coordinates: number[];
-        name: string;
-    }[];
-    isActive: boolean;
-};
 
 const pathLayerStyles: LayerProps & Record<string, any> = {
     type: "line",
@@ -44,60 +19,14 @@ const pathLayerStyles: LayerProps & Record<string, any> = {
     },
 };
 
-export default function Welcome({
-    mapAccessToken,
-}: {
-    mapAccessToken: string;
-}) {
+export default function Welcome({ mapAccessToken }: { mapAccessToken: string }) {
     const [viewState, setViewState] = useState({
         longitude: 76.93,
         latitude: 8.51,
         zoom: 12.5,
     });
-    const [circularsData, setCircularsData] = useState<CircularData[]>([]);
 
-    const isAllActive = useMemo(() => {
-        return circularsData.every((circular) => circular.isActive);
-    }, [circularsData]);
-
-    useEffect(() => {
-        const generatePathLayer = async () => {
-            const circularNames = Object.keys(circulars) as CircularName[];
-            let segments: Coordinates[][] = circularNames.map((key) =>
-                getCircularCoordinates(key)
-            );
-
-            // Add first stop as last stop to make it circular
-            segments = segments.map((stops) => [...stops, stops[0]]);
-
-            const segmentPathPromises = segments.map((segment) =>
-                getMatch(mapAccessToken, segment)
-            );
-            const segmentPath = await Promise.all(segmentPathPromises);
-
-            const paths = segmentPath.map((path) =>
-                generateLayerFromGeometry(path?.geometry as any)
-            );
-            const stops = segments.map((segment) => getStopDetails(segment));
-            // const meta = segmentPath.map((path) => path?.journey);
-
-            const circularsData = circularNames.map((name, index) => {
-                const circularData = {
-                    name,
-                    color: circularColors[name],
-                    path: paths[index],
-                    stops: stops[index],
-                    isActive: true,
-                };
-
-                return circularData;
-            });
-
-            setCircularsData(circularsData);
-        };
-
-        generatePathLayer();
-    }, [circulars, mapAccessToken]);
+    const { circulars, toggleCircularPath, isAllActive, geoJson: circularsGeojson } = useCirculars(mapAccessToken);
 
     return (
         <>
@@ -105,41 +34,24 @@ export default function Welcome({
 
             <div className="h-full max-h-screen min-h-screen w-full flex-row-reverse">
                 <section className="map-container relative flex flex-1">
-                    <ul className="tab tabs-boxed absolute left-0 top-0 z-10 my-3 h-auto gap-2 bg-opacity-60 font-bold opacity-90 transition-all md:left-[40vw]">
+                    <ul className="tab tabs-boxed absolute left-0 top-0 z-10 my-3 flex h-auto flex-wrap gap-2 bg-opacity-60 font-bold opacity-90 transition-all md:left-[40vw]">
                         <li key={`circular-all`}>
                             <button
-                                onClick={() => {
-                                    setCircularsData((prevData) => {
-                                        const newData = [...prevData];
-                                        newData.forEach((circular) => {
-                                            circular.isActive = !isAllActive;
-                                        });
-                                        return newData;
-                                    });
-                                }}
-                                className={`tab tab-active !px-4 !py-1 text-xs capitalize transition-all ${
-                                    isAllActive ? "tab-active" : "opacity-50"
-                                }`}
+                                onClick={() => toggleCircularPath()}
+                                className={`tab tab-active !px-4 !py-1 text-xs capitalize transition-all ${isAllActive ? "tab-active" : "opacity-50"
+                                    }`}
                             >
                                 All
                             </button>
                         </li>
-                        {circularsData.map((circular, i: number) => (
+                        {circulars.map((circular, i: number) => (
                             <li key={`circular-${i}`}>
                                 <button
-                                    onClick={() => {
-                                        setCircularsData((prevData) => {
-                                            const newData = [...prevData];
-                                            newData[i].isActive =
-                                                !newData[i].isActive;
-                                            return newData;
-                                        });
-                                    }}
-                                    className={`tab tab-active !px-4 !py-1 text-xs capitalize ${
-                                        circular.isActive
-                                            ? "tab-active"
-                                            : "opacity-50"
-                                    }`}
+                                    onClick={() => toggleCircularPath(i)}
+                                    className={`tab tab-active !px-4 !py-1 text-xs capitalize ${circular.isActive
+                                        ? "tab-active"
+                                        : "opacity-50"
+                                        }`}
                                     style={{
                                         backgroundColor: circular.color,
                                     }}
@@ -151,12 +63,12 @@ export default function Welcome({
                     </ul>
 
                     <BusRouteMap
-                        circulars={circulars}
+                        circulars={circularsGeojson}
                         {...viewState}
                         onMove={(evt) => setViewState(evt.viewState)}
                         mapboxAccessToken={mapAccessToken}
                     >
-                        {circularsData
+                        {circulars
                             .filter((circular) => circular.isActive)
                             .map((circular, i: number) => {
                                 const layerStyles = { ...pathLayerStyles };
---
+++ resources/js/utils/hooks/userCirculars.ts
@@ -0,0 +1,111 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { getMatch } from "../mapbox-api";
+import {
+    circularColors,
+    CircularName,
+    circulars as circularGeoJson,
+    Coordinates,
+    generateLayerFromGeometry,
+    getCircularCoordinates,
+    getStopDetails,
+} from "./../geoJson";
+
+type CircularData = {
+    name: CircularName;
+    color: string;
+    path: {
+        type: string;
+        features: {
+            type: string;
+            properties: Object;
+            geometry: Object;
+        }[];
+    };
+    stops: {
+        coordinates: number[];
+        name: string;
+    }[];
+    isActive: boolean;
+};
+
+const useCirculars = (mapAccessToken: string) => {
+    const [circularsData, setCircularsData] = useState<CircularData[]>([]);
+
+    const isAllActive = useMemo(() => {
+        return circularsData.every((circular) => circular.isActive);
+    }, [circularsData]);
+
+    const toggleCircularPath = useCallback(
+        (index?: number) => {
+            // TODO: use some unique ID instead of index
+            if (index !== undefined) {
+                setCircularsData((prevData) => {
+                    const newData = [...prevData];
+                    newData[index].isActive = !newData[index].isActive;
+                    return newData;
+                });
+
+                return;
+            }
+
+            setCircularsData((prevData) => {
+                const newData = [...prevData];
+                newData.forEach((circular) => {
+                    circular.isActive = !isAllActive;
+                });
+                return newData;
+            });
+        },
+        [isAllActive]
+    );
+
+    useEffect(() => {
+        const generatePathLayer = async () => {
+            const circularNames = Object.keys(
+                circularGeoJson
+            ) as CircularName[];
+            let segments: Coordinates[][] = circularNames.map((key) =>
+                getCircularCoordinates(key)
+            );
+
+            // Add first stop as last stop to make it circular
+            segments = segments.map((stops) => [...stops, stops[0]]);
+
+            const segmentPathPromises = segments.map((segment) =>
+                getMatch(mapAccessToken, segment)
+            );
+            const segmentPath = await Promise.all(segmentPathPromises);
+
+            const paths = segmentPath.map((path) =>
+                generateLayerFromGeometry(path?.geometry as any)
+            );
+            const stops = segments.map((segment) => getStopDetails(segment));
+            // const meta = segmentPath.map((path) => path?.journey);
+
+            const circularsData = circularNames.map((name, index) => {
+                const circularData = {
+                    name,
+                    color: circularColors[name],
+                    path: paths[index],
+                    stops: stops[index],
+                    isActive: true,
+                };
+
+                return circularData;
+            });
+
+            setCircularsData(circularsData);
+        };
+
+        generatePathLayer();
+    }, [circularGeoJson, mapAccessToken]);
+
+    return {
+        circulars: circularsData,
+        geoJson: circularGeoJson,
+        toggleCircularPath,
+        isAllActive,
+    };
+};
+
+export default useCirculars;
